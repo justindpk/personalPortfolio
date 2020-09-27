889842af77d358b71afc051da169e485ce9621d7
Justin <jdapatrick@gmail.com>
2020-09-27 13:24:26 -0700

    Files needed to construct website.
    first upload

---
+++ doBetter/navPage/homePg/assets/js/personals.js
@@ -0,0 +1,40 @@
+/* Typed.js functions */
+var typed = new Typed(".type", {
+    strings: [
+        "Clean.",
+        "Intuitive.",
+        "Enjoyable.",
+        ],
+        typeSpeed: 60,
+        backSpeed: 100,
+        loop: true,
+    
+});
+
+var career = new Typed(".career", {
+    strings: [
+        "front-end",
+        "software",
+        "web",
+        "product design",
+        ],
+        typeSpeed: 50,
+        backSpeed: 100,
+        loop: true,
+    
+});
+
+var typed = new Typed(".type2", {
+    strings: [
+        "novel applications.",
+        "frameworks.",
+        "design patterns.",
+        "UI/UX principles.",
+        ],
+        typeSpeed: 60,
+        backSpeed: 100,
+        loop: true,
+    
+});
+		
+/* Page Transition Function */
---
+++ doBetter/navPage/homePg/assets2/js/personals.js
@@ -0,0 +1,41 @@
+/* Typed.js functions */
+var stack = new Typed(".stack", {
+    strings: [
+        "novel applications.",
+        "frameworks.",
+        "design patterns.",
+        "UI/UX principles.",
+        ],
+        typeSpeed: 60,
+        backSpeed: 100,
+        loop: true,
+});
+
+var typed = new Typed(".type", {
+    strings: [
+        "Clean.",
+        "Intuitive.",
+        "Enjoyable.",
+        ],
+        typeSpeed: 60,
+        backSpeed: 100,
+        loop: true,
+    
+});
+
+var career = new Typed(".career", {
+    strings: [
+        "front-end",
+        "software",
+        "web",
+        "big data",
+        ],
+        typeSpeed: 50,
+        backSpeed: 100,
+        loop: true,
+    
+});
+
+
+
+/* Page Transition Function */
